1158bf5770abf4fbdb90cf488f355e679bfe5393
Maria Racariu <90383391+MariaRacariu@users.noreply.github.com>
2024-01-17 12:40:15 +0100

    added firebase data

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -7,11 +7,9 @@ import { initializeApp } from "firebase/app";
 const firebaseConfig = {
   apiKey: "AIzaSyDzTVnnpG4-KdDMJ6wZQvsvf3k1tPHK8h8",
   authDomain: "slutprojekt-versionhantering.firebaseapp.com",
+  databaseURL: "https://slutprojekt-versionhantering-default-rtdb.europe-west1.firebasedatabase.app",
   projectId: "slutprojekt-versionhantering",
   storageBucket: "slutprojekt-versionhantering.appspot.com",
   messagingSenderId: "449173684587",
   appId: "1:449173684587:web:c5a004cb69a21599a3cc2e"
 };
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
