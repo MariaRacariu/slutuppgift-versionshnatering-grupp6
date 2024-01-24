82659b01058bd5695975d63a6a32c761b1aded98
MariaRacariu <90383391+MariaRacariu@users.noreply.github.com>
2024-01-24 12:13:29 +0100

    small button fix

--- js/modules/header.js
+++ js/modules/header.js
@@ -11,14 +11,14 @@ export function createHeader(auth, provider, user) {
     header.append(logoutButton);
   } else {
     header.append("Welcome, stranger!");
+    let buttonContainer = $("<div class='buttonContainer'></div>");
+    header.append(buttonContainer);
+
     let loginButton = $("<button class='btn button primary'>Login with Google</button>");
     loginButton.on("click", () => signInWithGoogle(auth, provider));
-    header.append(loginButton);
+    buttonContainer.append(loginButton);
 
-    // Javad feature (Added button with info)
-    var button = $("<button > Read me!</button > ");
-    button.on("click", () => alert("Warning! If you don't log in, you will not have the right to delete or edit your messages. If your messages are dangerous or offensive they will be deleted by admin!"));
-    header.append(button);
+
   }
 
   return header;
