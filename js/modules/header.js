98df9402a3523c4cd5f1d00dfe00adb0ccad286c
MariaRacariu <90383391+MariaRacariu@users.noreply.github.com>
2024-01-24 11:46:28 +0100

    Javad's feature

--- js/modules/header.js
+++ js/modules/header.js
@@ -1,5 +1,7 @@
 import { signInWithGoogle, signOutFromGoogle } from "../auth.js";
 
+
+
 export function createHeader(auth, provider, user) {
   const header = $("<div class='header'></div>");
   if (user) {
@@ -12,6 +14,12 @@ export function createHeader(auth, provider, user) {
     let loginButton = $("<button class='btn button primary'>Login with Google</button>");
     loginButton.on("click", () => signInWithGoogle(auth, provider));
     header.append(loginButton);
+
+    // Javad feature (Added button with info)
+    var button = $("<button > Read me!</button > ");
+    button.on("click", () => alert("Warning! If you don't log in, you will not have the right to delete or edit your messages. If your messages are dangerous or offensive they will be deleted by admin!"));
+    header.append(button);
   }
+
   return header;
 }
