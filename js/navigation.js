ccd2ff7790cb93c2fc2a1654c426372f99b33669
Maria Racariu <90383391+MariaRacariu@users.noreply.github.com>
2024-01-24 18:39:01 +0100

    added theme preference local storage

--- js/navigation.js
+++ js/navigation.js
@@ -2,26 +2,47 @@ const navCloseIcon = document.getElementById("navCloseIcon");
 const navBarIcon = document.getElementById("navBarIcon");
 const nav = document.getElementById("mainNav");
 
+
 const themeSwitcher = document.getElementById("theme-switcher");
+themeSwitcher.addEventListener("click", toggleTheme);
 
 const doc = document.documentElement;
 const theme = () => doc.getAttribute("data-theme");
+
 if (!theme()) {
     doc.setAttribute("data-theme", "light");
     themeSwitcher.innerHTML = "light";
 }
+//Checks if localStorage is saved
+if (localStorage.getItem("theme") === "light") {
+    localStorage.setItem("theme", "light");
+    doc.setAttribute("data-theme", "light");
+    themeSwitcher.innerHTML = "Dark";
+} else if (localStorage.getItem("theme") === "dark") {
+    doc.setAttribute("data-theme", "dark");
+    localStorage.setItem("theme", "dark");
+    themeSwitcher.innerHTML = "Light";
+} else {
+    localStorage.setItem("theme", "light");
+    doc.setAttribute("data-theme", "light");
+    themeSwitcher.innerHTML = "Dark";
+}
 
 function toggleTheme() {
     if (theme() === "light") {
+        // Dark theme
         doc.setAttribute("data-theme", "dark");
+        localStorage.setItem("theme", "dark");
         themeSwitcher.innerHTML = "Light";
     } else if (theme() === "dark") {
+        // Light Theme
+        localStorage.setItem("theme", "light");
         doc.setAttribute("data-theme", "light");
         themeSwitcher.innerHTML = "Dark";
     }
 }
 
-themeSwitcher.addEventListener("click", toggleTheme);
+
 
 navCloseIcon.addEventListener("click", () => {
     nav.classList.remove("open");
