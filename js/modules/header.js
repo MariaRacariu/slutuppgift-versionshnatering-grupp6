34ceadf8e6b0a183e66cc1430e0a49ef9415f75b
MariaRacariu <90383391+MariaRacariu@users.noreply.github.com>
2024-01-25 10:34:06 +0100

    css fix path nav image

--- js/modules/header.js
+++ js/modules/header.js
@@ -18,6 +18,7 @@ export function createHeader(auth, provider, user) {
     loginButton.on("click", () => signInWithGoogle(auth, provider));
     buttonContainer.append(loginButton);
 
+    // Javad feature
     var button = $("<button class='btn button delete'> Read me!</button > ");
     button.on("click", () => alert("Warning! If you don't log in, you will not have the right to delete or edit your messages. If your messages are dangerous or offensive they will be deleted by admin!"));
     buttonContainer.append(button);
